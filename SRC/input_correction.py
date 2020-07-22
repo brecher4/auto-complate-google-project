e08facdb460b7e228ad3d8bd497159127f9020ff
brecher4 <bt@sem-wolf>
2020-07-22 13:48:44 +0300

    add get_completions_with_correction function

--- SRC/input_correction.py
+++ SRC/input_correction.py
@@ -20,8 +20,31 @@ def character_replacement(substr, num_sentences):
 
                 nonlocal suitable_completions
                 suitable_completions = get_sentences_data()[index_begin:index_end]
+                num_sentences -= num_suitables
+            
             if num_suitables > num_sentences:
                 break
 
         if num_suitables > num_sentences:
                 break
+
+
+def deleting_character(substr, num_sentences):
+    return []
+
+
+def adding_character(substr, num_sentences):
+    return []
+
+
+def get_completions_with_correction(substr, num_sentences):
+    correction_list = []
+    correction_list += character_replacement(substr, num_sentences)
+    correction_list += deleting_character(substr, num_sentences)
+    correction_list += adding_character(substr, num_sentences)
+
+    correction_list = sorted(correction_list, key=lambda k: k["score"], reverse=True)
+    len_correction_list = len(correction_list)
+    index_end = 5 if len_correction_list > 5 else len_correction_list
+    
+    return correction_list[:index_end]
