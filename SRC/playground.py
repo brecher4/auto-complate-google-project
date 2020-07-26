153631bc43241d6b97860253ac15371d88410bf6
brecher4 <bt@sem-wolf>
2020-07-26 03:39:56 +0300

    add some experiences

--- SRC/playground.py
+++ SRC/playground.py
@@ -55,7 +55,38 @@
 # }
 
 
+# import glob
 
+
+# list_ = []
+
+# for filename in glob.glob('data/technology_texts/...../*/*.txt'):
+#    list_ += [filename]
+
+# print(list_, len(list_))
+
+# import os
+
+# i = 0
+
+# def scan_folder(parent):
+#     # iterate over all the files in directory 'parent'
+#     for file_name in os.listdir(parent):
+#         if file_name.endswith(".txt"):
+#             # if it's a txt file, print its name (or do whatever you want)
+#             global i
+#             print(i,file_name)
+#             i += 1
+#         else:
+#             current_path = "".join((parent, "/", file_name))
+#             if os.path.isdir(current_path):
+#                 # if we're checking a sub-directory, recall this method
+#                 scan_folder(current_path)
+
+# scan_folder("data/technology_texts")
+
+# import string
+# print(string.printable.replace('a',''), len(string.printable.replace('a','')))
 
 #TREI
 # page = {}
@@ -74,3 +105,21 @@
 # #
 # #arrived to end
 # suitable_sen = 
+
+
+# import shelve
+
+# s = shelve.open('t.db')
+# try:
+#     s['key1'] = { 'int': 10, 'float':9.5, 'string':'Sample data' }
+# finally:
+#     s.close()
+
+
+# s = shelve.open('t.db')
+# try:
+#     existing = s['key1']
+# finally:
+#     s.close()
+
+# print(existing)
